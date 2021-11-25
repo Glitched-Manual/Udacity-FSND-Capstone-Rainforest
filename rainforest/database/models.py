c5961e8428b38598be942e0c00b1e30f3f77f9ce
Glitched-Manual <popcorneater720@gmail.com>
2021-11-25 17:58:55 -0500

    added class notes to models.py

--- rainforest/database/models.py
+++ rainforest/database/models.py
@@ -4,4 +4,45 @@ import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
+# User
+"""
+The class to carry the user's profile
 
+attributes:
+
+Orders (array of odred ids ) [int]
+"""
+
+# Order
+"""
+The class that contains all the orderItems for a users transaction
+
+attributes:
+
+User.id int
+orderItems [int]
+
+"""
+
+# OrderItem
+"""
+An Item within the order with a quantity (delete if quatity is zero)
+
+attributes:
+Order.id
+Product.id
+ItemQuantity int
+"""
+
+
+# Product
+"""
+the class for ever item that is for sale
+
+attributes:
+
+- name
+- description
+- price
+
+"""
