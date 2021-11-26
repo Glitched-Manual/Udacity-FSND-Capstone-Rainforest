954210a5d5b93dcad628ba9a1dbd09def5bfb47a
Glitched-Manual <popcorneater720@gmail.com>
2021-11-25 20:06:44 -0500

    Developing models

--- rainforest/database/models.py
+++ rainforest/database/models.py
@@ -3,6 +3,20 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.orm import backref
+
+
+database_path = os.environ['DATABASE_URL_FIXED']
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
 
 # User
 """
@@ -12,6 +26,16 @@ attributes:
 
 Orders (array of odred ids ) [int]
 """
+
+class User(db.Model):
+    __tablename__ = 'User'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    number = db.Column(db.String, nullable=False)
+    orders = db.Column(db.Array(db.relationship('Order', backref='user', lazy=True)))
+
 
 # Order
 """
@@ -23,6 +47,12 @@ User.id int
 orderItems [int]
 
 """
+class Order(db.Model):
+    __tablename__ ='Order'
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_items = db.relationship('OrderItem', backref='order', lazy=True)
+    user_id = db.Column(db.Integer, db.Foriegn_key('User.id'), nullable=False)
 
 # OrderItem
 """
