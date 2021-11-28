6ad2c833fa6c39c0efef83ae9699fa36e2996a97
Glitched-Manual <popcorneater720@gmail.com>
2021-11-27 21:03:32 -0500

    developing app.py ad tests

--- rainforest/app.py
+++ rainforest/app.py
@@ -2,6 +2,8 @@ import os ,sys
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
+from .database.models import db_drop_and_create_all, setup_db
+from .auth.auth import AuthError, requires_auth
 
 def create_app(test_config=None):
   # create and configure the app
@@ -9,8 +11,53 @@ def create_app(test_config=None):
   CORS(app)
 
   return app
-
+ 
 APP = create_app()
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
+
+
+
+# Error Handling
+
+
+@APP.errorhandler(404)
+def unprocessable(error):
+    return jsonify({
+                    "success": False,
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
+'''
+@TODO implement error handler for 404
+    error handler should conform to general task above
+'''
+@APP.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+'''
+@TODO implement error handler for AuthError
+    error handler should conform to general task above
+'''
+@APP.errorhandler(401)
+def unprocessable(error):
+    return jsonify({
+                    "success": False,
+                    "error": 401,
+                    "message": "resource not found"
+                    }), 401
+
+
+@APP.errorhandler(AuthError)
+def handle_auth_error(ex):
+    return jsonify({
+                    "success": False,
+                    "error": ex.status_code,
+                    "message": ex.error
+                    }), ex.status_code
